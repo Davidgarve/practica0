15bdcf58f4c3e327c5813597e0dc40b3b978ebe2
Davidgarve <davidgaver2004@gmail.com>
2023-09-19 11:38:14 +0100

    updated Chessboard 2.0

--- 5. ArrayList and 2D Arrays/2D Arrays/5.3 Chessboard/src/Chessboard.java
+++ 5. ArrayList and 2D Arrays/2D Arrays/5.3 Chessboard/src/Chessboard.java
@@ -1,20 +1,16 @@
 public class Chessboard
-        //Clase para elaborar tablero de ajederez
+//Clase para elaborar tablero de ajederez
 {
    public static void main(String[] args)
    {
       char[][] chessboard = new char[8][8];
       for (int y = 0; y < chessboard.length; y++)
-      {
-         for (int x = 0; x < chessboard.length; x++)
-         {
-            if ((x+y)%2 == 0)
+         for (int x = 0; x < chessboard.length; x++) {
+            if ((x + y) % 2 == 0)
                chessboard[y][x] = '\u25A1';
             else
                chessboard[y][x] = '\u25A0';
          }
-
-      }
       for (char[] row: chessboard)
       {
          for (char position: row)
