1450663c78f71bdc01e7cd734f11331dd81d27bd
Davidgarve <davidgaver2004@gmail.com>
2023-09-19 11:10:53 +0100

    first commit

---
+++ 4. Arrays and String Methods/String Methods/4.1 Get Shortest Word/src/GetShortestWord.java
@@ -0,0 +1,14 @@
+public class GetShortestWord
+{
+    //code the method getShortestWord that accepts two words and returns the shortest one
+    public static String getShortestWord( String first, String second){
+        if (first.length() > second.length()){
+            return second;
+        }
+        else{
+            return first;
+        }
+    }
+    /* Code a method that returns the shortest word of the two inputs*/
+}
+
